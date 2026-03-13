ad34b8fd0f54f3e05538c243e905572c2aa5e519
D1mKash <vinhgamesvg@gmail.com>
2026-03-12 22:45:44 -0400

    aizen.lua

--- aizen.lua
+++ aizen.lua
@@ -4,7 +4,7 @@ local UserInputService = game:GetService("UserInputService")
 local VirtualInputManager = game:GetService("VirtualInputManager")
 
 local inputConnection
-local internalPress = false
+local ignoreInput = false
 
 local function pressKey(key)
     VirtualInputManager:SendKeyEvent(true, key, false, game)
@@ -12,7 +12,7 @@ local function pressKey(key)
 end
 
 local function randomDelay()
-    return 0.04 + math.random() * (0.1 - 0.04)
+    return math.random(40,100) / 1000 -- 0.04 - 0.10
 end
 
 function module.Start()
@@ -21,25 +21,29 @@ function module.Start()
         inputConnection:Disconnect()
     end
 
-    inputConnection = UserInputService.InputBegan:Connect(function(input, gpe)
+    inputConnection = UserInputService.InputBegan:Connect(function(input,gpe)
 
         if gpe then return end
-        if internalPress then return end
+        if ignoreInput then return end
 
         if input.KeyCode == Enum.KeyCode.Two then
 
-            internalPress = true
-            task.delay(randomDelay(), function()
+            ignoreInput = true
+
+            task.delay(randomDelay(),function()
                 pressKey(Enum.KeyCode.Three)
-                internalPress = false
+                task.wait(0.05)
+                ignoreInput = false
             end)
 
         elseif input.KeyCode == Enum.KeyCode.Three then
 
-            internalPress = true
-            task.delay(randomDelay(), function()
+            ignoreInput = true
+
+            task.delay(randomDelay(),function()
                 pressKey(Enum.KeyCode.Two)
-                internalPress = false
+                task.wait(0.05)
+                ignoreInput = false
             end)
 
         end
