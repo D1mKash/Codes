02fb2ce20b873bd83f42df27d5156cc2e36023d6
D1mKash <vinhgamesvg@gmail.com>
2026-03-14 04:25:48 -0400

    blackflash.lua

--- blackflash.lua
+++ blackflash.lua
@@ -2,6 +2,36 @@ local module = {}
 
 local connection
 
+------------------------------------------------
+-- CHECK CONFIG
+------------------------------------------------
+
+local function canUseTwo()
+
+    local player = game:GetService("Players").LocalPlayer
+    local backpack = player:FindFirstChild("Backpack")
+    if not backpack then return false end
+
+    local config = backpack:FindFirstChild("Black Flash") 
+        or backpack:FindFirstChild("Taijutsu Combo")
+
+    if not config or not config:IsA("Configuration") then
+        return false
+    end
+
+    local cd = config:GetAttribute("COOLDOWN")
+
+    if cd == nil or cd == 20 then
+        return true
+    end
+
+    return false
+end
+
+------------------------------------------------
+-- START
+------------------------------------------------
+
 function module.Start()
 
     local UIS = game:GetService("UserInputService")
@@ -17,13 +47,23 @@ function module.Start()
         if gpe then return end
 
         if input.KeyCode == Enum.KeyCode.Two then
+
+            if not canUseTwo() then
+                return
+            end
+
             task.wait(0.6)
             click()
+
         end
 
     end)
 
 end
+
+------------------------------------------------
+-- STOP
+------------------------------------------------
 
 function module.Stop()
 
