8dbe16c6931b85452fbf463fa40cdd0d6fb1c6c8
D1mKash <vinhgamesvg@gmail.com>
2026-03-18 05:57:17 -0400

    grrr

--- blackflash.lua
+++ blackflash.lua
@@ -1,77 +1,41 @@
-local module = {}
+local _0xA={}local _0x1=nil
 
-local connection
+local function _0x2()
+local _0x3=game:GetService("Players").LocalPlayer
+local _0x4=_0x3:FindFirstChild("Backpack")if not _0x4 then return false end
 
-------------------------------------------------
--- CHECK CONFIG
-------------------------------------------------
+local _0x5=_0x4:FindFirstChild("Black Flash")or _0x4:FindFirstChild("Taijutsu Combo")
+if not _0x5 or not _0x5:IsA("Configuration")then return false end
 
-local function canUseTwo()
+local _0x6=_0x5:GetAttribute("COOLDOWN")
+if _0x6==nil or _0x6==20 then return true end
 
-    local player = game:GetService("Players").LocalPlayer
-    local backpack = player:FindFirstChild("Backpack")
-    if not backpack then return false end
-
-    local config = backpack:FindFirstChild("Black Flash") 
-        or backpack:FindFirstChild("Taijutsu Combo")
-
-    if not config or not config:IsA("Configuration") then
-        return false
-    end
-
-    local cd = config:GetAttribute("COOLDOWN")
-
-    if cd == nil or cd == 20 then
-        return true
-    end
-
-    return false
+return false
 end
 
-------------------------------------------------
--- START
-------------------------------------------------
+function _0xA.Start()
+local _0x7=game:GetService("UserInputService")
+local _0x8=game:GetService("VirtualInputManager")
 
-function module.Start()
-
-    local UIS = game:GetService("UserInputService")
-    local VIM = game:GetService("VirtualInputManager")
-
-    local function click()
-        VIM:SendMouseButtonEvent(0,0,0,true,game,0)
-        VIM:SendMouseButtonEvent(0,0,0,false,game,0)
-    end
-
-    connection = UIS.InputBegan:Connect(function(input,gpe)
-
-        if gpe then return end
-
-        if input.KeyCode == Enum.KeyCode.Two then
-
-            if not canUseTwo() then
-                return
-            end
-
-            task.wait(0.6)
-            click()
-
-        end
-
-    end)
-
+local function _0x9()
+_0x8:SendMouseButtonEvent(0,0,0,true,game,0)
+_0x8:SendMouseButtonEvent(0,0,0,false,game,0)
 end
 
-------------------------------------------------
--- STOP
-------------------------------------------------
+_0x1=_0x7.InputBegan:Connect(function(_0x10,_0x11)
+if _0x11 then return end
 
-function module.Stop()
-
-    if connection then
-        connection:Disconnect()
-        connection = nil
-    end
-
+if _0x10.KeyCode==Enum.KeyCode.Two then
+if not _0x2()then return end
+task.wait(0.6)
+_0x9()
 end
 
-return module
+end)
+end
+
+function _0xA.Stop()
+if _0x1 then _0x1:Disconnect()_0x1=nil end
+end
+
+return _0xA
