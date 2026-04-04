c0176785f4da70083a384052a1e0a3031f84fe24
D1mKash <vinhgamesvg@gmail.com>
2026-04-04 17:59:35 -0400

    Update haha.lua

--- haha.lua
+++ haha.lua
@@ -1,5 +1,10 @@
 local m={}local p=game:GetService("Players")local v=game:GetService("VirtualInputManager")local u=game:GetService("UserInputService")local pl=p.LocalPlayer
-local a={["1461157246"]={{key=Enum.KeyCode.Three,delay=0},{key=Enum.KeyCode.Q,delay=0.01}}}
+local a={
+	["1461157246"]={{key=Enum.KeyCode.Three,delay=0},{key=Enum.KeyCode.Q,delay=0.01}},
+	["1461137417"]={{key=Enum.KeyCode.Three,delay=0},{key=Enum.KeyCode.Q,delay=0.01}},
+	["1461252313"]={{key=Enum.KeyCode.Three,delay=0},{key=Enum.KeyCode.Q,delay=0.01}},
+  ["1461127258"]={{key=Enum.KeyCode.Three,delay=0},{key=Enum.KeyCode.Q,delay=0.01}}
+}
 local c={}local r=false
 local function k(kc)pcall(function()v:SendKeyEvent(true,kc,false,game)task.wait(0.01)v:SendKeyEvent(false,kc,false,game)end)end
 local function h(anim)if not anim then return end local s,e=pcall(function()return anim.AnimationPlayed:Connect(function(t)if not r or not t or not t.Animation then return end local i=t.Animation.AnimationId if not i or i=="" then return end for id,acts in pairs(a)do if string.find(i,id)then for _,act in ipairs(acts)do if act.delay and act.delay>0 then task.wait(act.delay)end k(act.key)end break end end end)end)if s and e then table.insert(c,e)end end
