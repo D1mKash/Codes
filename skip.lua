52a57848e232aab578ea400c04a5d7c8fd84abb2
D1mKash <vinhgamesvg@gmail.com>
2026-05-08 20:31:21 -0400

    skip...

--- skip.lua
+++ skip.lua
@@ -41,12 +41,10 @@ local function isValidTarget(model)
 
 	local myChar = player.Character
 
-	-- do not target yourself
 	if model == myChar or model.Name == player.Name then
 		return false
 	end
 
-	-- if model belongs to a player, check team
 	local targetPlayer = Players:FindFirstChild(model.Name)
 
 	if targetPlayer then
@@ -117,7 +115,6 @@ local function teleportBehindNearest(maxRange)
 	local targetRoot = getRoot(target)
 	if not targetRoot then return false end
 
-	-- 3 studs behind target
 	local behind = targetRoot.CFrame * CFrame.new(0, 0, 3)
 	local goal = CFrame.new(behind.Position, targetRoot.Position)
 
@@ -142,6 +139,7 @@ local function delayedTeleport(delayTime, maxRange, clickAfterTeleport)
 		local teleported = teleportBehindNearest(maxRange)
 
 		if teleported and clickAfterTeleport then
+			task.wait(0.753)
 			leftClick(0.1)
 		end
 	end)
@@ -161,7 +159,7 @@ function m.Start()
 
 		if input.KeyCode == Enum.KeyCode.Two then
 			if hasInBackpack("Erasure Ω") then
-				delayedTeleport(1.6, 100, true)
+				delayedTeleport(1.963, 100, true)
 			end
 		end
 
