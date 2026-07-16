2866941ee8a9e8d7cf8df6e7bf468394e59aea35
D1mKash <vinhgamesvg@gmail.com>
2026-07-15 20:47:02 -0400

    haha.lua

--- haha.lua
+++ haha.lua
@@ -375,8 +375,8 @@ local function startInfinityScan()
 					pressKey(Enum.KeyCode.One)       -- 1
 					task.wait(1.7)                   -- changed from 1.0 to 1.7
 
-					-- Disable gravity
-					workspace.Gravity = 0
+					-- Lowers gravity
+					workspace.Gravity = 40
 
 					-- Apply upward velocity to simulate jump (now gravity is off)
 					local char = getCharacter()
