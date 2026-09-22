e9df5bca3175ea12d2f234df1e3d0bebbfdc994d
D1mKash <vinhgamesvg@gmail.com>
2026-09-22 06:06:01 -0400

    Update head.lua

--- head.lua
+++ head.lua
@@ -132,7 +132,7 @@ local function smoothFollow(targetModel)
 
 		local goal = CFrame.new(pos, pos + look)
 
-		myRoot.CFrame = myRoot.CFrame:Lerp(goal, 0.01)
+		myRoot.CFrame = myRoot.CFrame:Lerp(goal, 0.03)
 
 		R.Heartbeat:Wait()
 	end
